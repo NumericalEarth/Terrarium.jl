a2839ed8c7c5e6b445726482161c536baac24e0f
Brian Groenke <brian.groenke@pik-potsdam.de>
2026-09-18 14:43:03 +0200

    Fix errors in BC tests

--- test/boundary_conditions.jl
+++ test/boundary_conditions.jl
@@ -1,8 +1,8 @@
 using Terrarium
-using Terrarium: StateVariables, prognostic, auxiliary, var, input, fill_halo_regions!, varname, vardims, XY, XYZ
+using Terrarium: StateVariables, prognostic, auxiliary, var, input, fill_halo_regions!, merge_boundary_conditions, varname, vardims, XY, XYZ
 using Test
 
-@testset "BC initialization" begin
+@testset "Field BC configuration" begin
     Nz = 10
     grid = ColumnGrid(UniformSpacing(Δz = 0.1, N = Nz))
     vars = variables(prognostic(:x, XYZ()), auxiliary(:y, XYZ()), input(:c, XY()))
@@ -14,13 +14,7 @@ using Test
     @test state.x.boundary_conditions.top == upperbc
     @test state.x.boundary_conditions.bottom == lowerbc
     set!(state.x, 0.5)
-    fill_halo_regions!(state)
-end
-
-@testset "Merge BCs" begin
-    Nz = 10
-    grid = ColumnGrid(UniformSpacing(Δz = 0.1, N = Nz))
-    vars = variables(prognostic(:x, XYZ()), auxiliary(:y, XYZ()), input(:c, XY()))
+    fill_halo_regions!(state.x, state)
     # check that halo matches boundary value that makes the face equal to 1
     @test state.x[1, 1, Nz + 1] == 1.5
     bc2 = (y = (top = ValueBoundaryCondition(var(:c, XY())), bottom = ValueBoundaryCondition(0.0)),)
@@ -30,6 +24,6 @@ end
     state = StateVariables(vars, grid; clock, boundary_conditions = merged_bcs)
     # check that we can set the input variable to modify the boundary condition
     set!(state.c, 1.0)
-    fill_halo_regions!(state)
+    fill_halo_regions!(state.y, state)
     @test state.x[1, 1, Nz + 1] == 2.0
 end
