c5698eb5fe10fbcc97a9e9693fb335e423dc7478
Brian Groenke <brian.groenke@pik-potsdam.de>
2026-09-25 16:49:30 +0200

    Change snow_fall domain to atmosphere in example

--- examples/extending/simple_snow_ddm.jl
+++ examples/extending/simple_snow_ddm.jl
@@ -57,7 +57,7 @@ end
 #
 Terrarium.variables(model::DegreeDaySnow{NF}) where {NF} = (
     Terrarium.input(:air_temperature, Terrarium.Atmosphere(XY()), default = NF(0), units = u"°C", desc = "Near-surface air temperature in °C"),
-    Terrarium.input(:snow_fall, Terrarium.Snow(XY()), default = NF(0), units = u"m/s", desc = "snow fall rate in m/s"),
+    Terrarium.input(:snow_fall, Terrarium.Atmosphere(XY()), default = NF(0), units = u"m/s", desc = "snow fall rate in m/s"),
     Terrarium.prognostic(:snow_storage, Terrarium.Snow(XY()), units = u"m", desc = "Snow water equivalent in m"),
 )
 
@@ -168,7 +168,7 @@ DisplayAs.PNG(heatmap(snow_climatology[:, 1], title = "Snowfall (m/s)"))
 # Now, we just need to define initialize everything correctly. As we are working with globally gridded data, we will define [`ColumnRingGrid`](@ref) based on the `global_grid` we already initialized. Then, we will load our inputs. For this, we will choose the January (so the first element) of our climatology files. When using them in [`InputSource`](@ref) be sure to choose the same name and units as used in the definitions of the dynamics before.
 
 grid = ColumnRingGrid(UniformSpacing(Δz = 0.1, N = 1), global_grid, land_sea_mask);
-sf_input = InputSource(grid, snow_climatology[:, 1], name = :snow_fall, units = u"m/s"; domain = Terrarium.Snow());
+sf_input = InputSource(grid, snow_climatology[:, 1], name = :snow_fall, units = u"m/s");
 lst_input = InputSource(grid, lst_climatology[:, 1], name = :air_temperature, units = u"°C")
 
 # As an initial condition, we just cover the whole Earth in deep snow (everywhere the same)!
